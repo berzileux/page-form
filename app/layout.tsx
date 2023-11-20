310d73b1f0f257c953858840e02b600f23fe286a
berzileux <bersileus@gmail.com>
2023-11-20 13:49:41 +0000

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs"
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import {Toaster} from "@/components/ui/toaster"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Pageforms',
+  description: 'Pageforms',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+	<ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+      	  <ThemeProvider attribute="class" defaultTheme="system"
+            enableSystem disableTransitionOnChange>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+      </body>
+    </html>
+    </ClerkProvider>
+  )
+}
